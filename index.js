1076d42c74effc86832ea662250f5d7be0e98930
bran259 <kiprop686@gmail.com>
2025-09-29 11:26:24 +0300

    added

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  isPalindrome("abba")     // true
+isPalindrome("racecar")  // true
+isPalindrome("a")        // true
+isPalindrome("robot")    // false
+isPalindrome("ab")       // false
+
 }
 
 /* 
   Add your pseudocode here
 */
+function isPalindrome(str) {
+  // reverse the string
+  const reversed = str.split("").reverse().join("");
+  // compare and return boolean
+  return str === reversed;
+}
+
+
+// Export for tests
+module.exports = isPalindrome;
+
 
 /*
   Add written explanation of your solution here
+  The function reverses the string and compares it to the original.
+
+If they match → returns true (palindrome).
+
+If not → returns false.
 */
 
 // You can run `node index.js` to view these console logs
